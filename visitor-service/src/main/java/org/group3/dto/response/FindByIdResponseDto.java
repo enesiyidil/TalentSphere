3f85708f4b00ecc82cc846103b50f0bf66c982da
Berresu Gülseçgin <gulsecginberresu@gmail.com>
2024-02-13 11:51:46 +0300

    Visitor service

---
+++ visitor-service/src/main/java/org/group3/dto/response/FindByIdResponseDto.java
@@ -0,0 +1,19 @@
+package org.group3.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FindByIdResponseDto {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+
+}
